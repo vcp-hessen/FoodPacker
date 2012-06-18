82c0efa94c5258abd522d25f93b1c7371f50704b
Tobias Klonk <tobias.klonk@tklonk.net>
2012-06-18 21:48:58 +0200

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodPacker::Application.config.secret_token = 'bd5f0f602324638d4695366d0abc5b848f88031584fcafd3cb415920ca71cace268026fb5362ed41a13548f91677e6081f16eb199570640a069226161a653a65'
