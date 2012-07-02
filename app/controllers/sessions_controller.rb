a4a96d2bc8584327f0ac048a10b6f6f12f968ba0
Tobias Klonk <tobias.klonk@tklonk.net>
2012-07-02 14:20:01 +0200

    localized user sign up and log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:login], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to_target_or_default root_url, :notice => "Logged in successfully."
+      redirect_to_target_or_default root_url, :notice => t("session.messages.logged_in")
     else
-      flash.now[:alert] = "Invalid login or password."
+      flash.now[:alert] = t("session.messages.invalid")
       render :action => 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "You have been logged out."
+    redirect_to root_url, :notice => t("session.messages.logged_out")
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, :notice => "Thank you for signing up! You are now logged in."
+      redirect_to root_url, :notice => t("users.messages.signed_up")
     else
       render :action => 'new'
     end
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update_attributes(params[:user])
-      redirect_to root_url, :notice => "Your profile has been updated."
+      redirect_to root_url, :notice => t("users.messages.updated")
     else
       render :action => 'edit'
     end
