7b9752148abfeb2febf02f3562cc0eda5b8c009c
Tobias Klonk <tobias.klonk@tklonk.net>
2012-07-03 22:56:49 +0200

    adding capistrano getting ready for deploy

--- Gemfile
+++ Gemfile
@@ -16,9 +16,13 @@ gem 'web-app-theme', '~> 0.8.0'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails',   '~> 3.1.4'
+  gem 'sass-rails', '3.1.4'
   gem 'coffee-rails', '~> 3.1.1'
   gem 'uglifier', '>= 1.0.3'
+end
+
+group :production do
+  gem 'fcgi'
 end
 
 gem 'jquery-rails'
@@ -30,7 +34,7 @@ gem 'jquery-rails'
 # gem 'unicorn'
 
 # Deploy with Capistrano
-# gem 'capistrano'
+gem 'capistrano'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/deploy.rb
@@ -0,0 +1,80 @@
+require 'bundler/capistrano'
+
+set :application, "FoodPacker"
+set :user, 'foodpack'
+
+role :web, "#{user}.draco.uberspace.de"                          # Your HTTP server, Apache/etc
+role :app, "#{user}.draco.uberspace.de"                          # This may be the same as your `Web` server
+role :db,  "#{user}.draco.uberspace.de", :primary => true # This is where Rails migrations will run
+
+set :deploy_to, "/home/#{user}/FoodPacker"
+set :deploy_via, :remote_cache
+set :scm, :git
+set :repository,  "ssh://git@code.tonklon.com/tonklon/foodpack.git"
+set :branch, 'development'
+set :scm_verbose, true
+set :use_sudo, false
+set :rails_env, :production
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "foodpack_rsa")]
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+
+end
+
+before "deploy:setup", "db:configure"
+after "deploy:update_code", "db:symlink"
+
+set(:database_username, user)
+set(:development_database) { application + "_development" }
+set(:test_database) { application + "_test" }
+set(:production_database) { user }
+
+namespace :db do
+  desc "Create database yaml in shared path"
+  task :configure do
+    set :database_password do
+      Capistrano::CLI.password_prompt "Database Password: "
+    end
+    
+    db_config = <<-EOF
+base: &base
+  adapter: mysql
+  encoding: utf8
+  username: #{database_username}
+  password: #{database_password}
+  socket: /var/lib/mysql/mysql.sock
+
+development:
+  database: #{development_database}
+  <<: *base
+
+test:
+  database: #{test_database}
+  <<: *base
+
+production:
+  database: #{production_database}
+  <<: *base
+    EOF
+
+    run "mkdir -p #{shared_path}/config"
+    put db_config, "#{shared_path}/config/database.yml"
+  end
+
+  desc "Make symlink for database yaml"
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+  end
+end
