5088781d71ad6aeedfe9e44826361a50c3a58a3c
Tobias Klonk <tobias.klonk@tklonk.net>
2012-07-05 17:46:23 +0200

    additional group validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
   
+  validates :participants_count, :numericality => { :only_integer => true , :greater_than => 0}
 end
--- test/unit/group_test.rb
+++ test/unit/group_test.rb
@@ -15,4 +15,30 @@ class GroupTest < ActiveSupport::TestCase
     assert group2.errors[:name].any?
   end
   
+  test "group participants_count must be an integer" do
+    group = groups(:group)
+    group.participants_count = "foo"
+    
+    assert group.invalid?
+    assert group.errors[:participants_count].any?
+  end
+  
+  test "participants_count must be greater 0" do
+    group = groups(:group)
+    group.participants_count = 0
+    
+    assert group.invalid?, "Accepted participants_count 0"
+    assert group.errors[:participants_count].any?, "No error for participants_count 0"
+    
+    group.participants_count = -1
+    
+    assert group.invalid?, "Accepted participants_count -1"
+    assert group.errors[:participants_count].any?, "No error for participants_count -1"
+    
+    group.participants_count = 1
+    
+    assert group.valid?, "Declined participants_count 1"
+    assert group.errors[:participants_count].empty?, "Error for participants_count 1"
+  end
+  
 end
