f3237690f57ceffb904b2071ba191ed23cab28e2
Tobias Klonk <tobias.klonk@tklonk.net>
2012-07-08 14:37:48 +0200

    fixed migration again

--- db/migrate/20120706185220_remove_receipts_from_meal.rb
+++ db/migrate/20120706185220_remove_receipts_from_meal.rb
@@ -1,11 +1,11 @@
 class RemoveReceiptsFromMeal < ActiveRecord::Migration
   def up
     remove_column :meals, :receipt_id
-    remove_column :meals, :atl_receipt_id
+    remove_column :meals, :alt_receipt_id
   end
 
   def down
-    add_column :meals, :atl_receipt_id, :integer
+    add_column :meals, :alt_receipt_id, :integer
     add_column :meals, :receipt_id, :integer
   end
 end
