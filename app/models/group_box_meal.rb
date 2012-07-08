b27c9d177e078208a33740a94740a7b86007a294
Tobias Klonk <tobias.klonk@tklonk.net>
2012-07-09 00:22:42 +0200

    group box meal calculates it's contents

--- app/models/group_box_meal.rb
+++ app/models/group_box_meal.rb
@@ -1,8 +1,23 @@
 class GroupBoxMeal < ActiveRecord::Base
   
   has_many :contents, class_name: "GroupBoxContents"
+  has_one :group, through: :group_box
   
   belongs_to :group_box
   belongs_to :meal
+
+  after_initialize do |group_box_meal|
+    
+    raise "initialized group box meal without meal_id" if group_box_meal.meal_id == nil
+    
+  end
+
+  def build_contents_from_ingredients_hunger_factor(ingredients,hunger_factor)
+    ingredients.each do |ingredient|
+      quantity = ingredient.quantity / 10 * participants_count * hunger_factor
+      contents.build(name: ingredient.product.name,quantity:quantity,unit:ingredient.product.unit)
+    end
+    contents
+  end
 
 end
