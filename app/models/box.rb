46521a321b61a27ec78cba16d0cf07edc5a92043
Tobias Klonk <tobias.klonk@tklonk.net>
2012-07-10 21:23:15 +0200

    changed box calculation
    is now even more efficient and should support multiple group_meals per meal

--- app/models/box.rb
+++ app/models/box.rb
@@ -5,12 +5,9 @@ class Box < ActiveRecord::Base
 
   def build_calculated_box_for_group(group)
     group_box = group_boxes.build(group: group)
-    meals.find_each(:include => :group_meals) do |meal|
-
-      group_meal = meal.group_meals.find_by_group_id(group.id, :include => [{:receipt => {:ingredients => :product}}, :meal])
-      next if group_meal == nil
+    GroupMeal.find_each(conditions:{group_id: group.id, meal_id: meal_ids}, :include => [{:receipt => {:ingredients => :product}}, :meal]) do |group_meal|
       
-      group_box_meal = group_box.group_box_meals.build(meal:meal)
+      group_box_meal = group_box.group_box_meals.build(meal:group_meal.meal)
       receipt = group_meal.receipt
       
       group_box_meal.participants_count = group_meal.meal_participants_count
@@ -24,25 +21,23 @@ class Box < ActiveRecord::Base
   end
   
   def build_calculated_boxes
-    meals.find_each(:include => :group_meals) do |meal|
-      meal.group_meals.find_each(:include => [{:receipt => {:ingredients => :product}}, :meal, :group]) do |group_meal|
+    GroupMeal.find_each(conditions:{meal_id: meal_ids}, :include => [{:receipt => {:ingredients => :product}}, :meal]) do |group_meal|
       
-        group_box = group_boxes.each do |group_box|
-          break group_box if group_box.group == group_meal.group
-        end
-        group_box = nil if group_box.kind_of?(Array)
-
-        group_box = group_boxes.build(group: group_meal.group) if group_box.nil?
-        group_box_meal = group_box.group_box_meals.build(meal:meal)
-        receipt = group_meal.receipt
-      
-        group_box_meal.participants_count = group_meal.meal_participants_count
-        group_box_meal.receipt_name = receipt.name
-      
-        hunger_factor = group_meal.meal_hunger_factor
-      
-        group_box_meal.build_contents_from_ingredients_hunger_factor receipt.ingredients, hunger_factor
+      group_box = group_boxes.each do |group_box|
+        break group_box if group_box.group == group_meal.group
       end
+      group_box = nil if group_box.kind_of?(Array)
+
+      group_box = group_boxes.build(group: group_meal.group) if group_box.nil?
+      group_box_meal = group_box.group_box_meals.build(meal:group_meal.meal)
+      receipt = group_meal.receipt
+    
+      group_box_meal.participants_count = group_meal.meal_participants_count
+      group_box_meal.receipt_name = receipt.name
+    
+      hunger_factor = group_meal.meal_hunger_factor
+    
+      group_box_meal.build_contents_from_ingredients_hunger_factor receipt.ingredients, hunger_factor
     end
     group_boxes
   end
