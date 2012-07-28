d28ad3143c4b5501703eebd88c95a20fb8cbafcc
Tobias Klonk <tobias.klonk@tklonk.net>
2012-07-28 16:04:10 +0200

    added better readable quantity

--- app/models/special.rb
+++ app/models/special.rb
@@ -2,4 +2,13 @@ class Special < ActiveRecord::Base
   belongs_to :group
   belongs_to :box
   belongs_to :product
+  
+  def human_readable_quantity
+    begin
+      ActionController::Base.helpers.number_to_human(quantity, :units => "units.#{product.unit}", :precision => 3)
+    rescue I18n::MissingTranslationData
+      ActionController::Base.helpers.number_to_human(quantity, :units => {unit: product.unit}, :precision => 3)
+    end
+  end
+  
 end
